8d1a952538dbf35d20c0748a481c28a615a017b7
LaurenJanicki <lauren.a.janicki@gmail.com>
2015-07-24 16:03:40 -0700

    Created databases

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,23 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
+  message_id INT AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(1000),
+  createdAt DATETIME,
+  userId INT,
+  roomId INT
+
   /* Describe your table here.*/
+);
+
+CREATE TABLE usernames (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20)
+);
+
+CREATE TABLE roomnames (
+  room_id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(40)
 );
 
 /* Create other tables and define schemas for them here! */
